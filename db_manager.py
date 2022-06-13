0a6de5db2b190674787f82a55aa3ceb595cc0495
Strovosky <strovosky@gmail.com>
2022-06-12 22:55:24 -0500

    second commit

--- db_manager.py
+++ db_manager.py
@@ -3,7 +3,7 @@ from sqlalchemy_utils import database_exists, create_database
 from models import Base
 
 
-engine = create_engine("mysql+pymysql://root:akabane33_@localhost:3306/university1")
+engine = create_engine("mysql+pymysql://your_user:your_password@localhost:3306/university1")
 
 if not database_exists(engine.url):
     create_database(engine.url)
